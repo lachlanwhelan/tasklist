253e3d89f21becd0b83bfdfb6d8340348694fa1d
Lachlan Whelan <lachlan.whelan1@gmail.com>
2022-09-11 12:20:08 +1000

    Initial setup - webpack config + folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Resets.scss';
+
+const App = () => {
+    return (
+        <main className='app'>
+            <h1>Tasklist 👍</h1>
+        </main>
+    )
+};
+    
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    
+    mode: 'production',
+    
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js',
+        publicPath: '/tasklist'
+    },
+
+    //devtool: 'source-map',
+    devtool: false,
+
+    devServer: {
+        static: './dist',
+        historyApiFallback: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            title: 'Tasklist'
+        })
+    ]
+}
